49308a64d201a26df3561bd0752671ad651a2052
toms74209200 <motomotomato@gmail.com>
2021-01-09 23:38:43 +0900

    [add]add sec. 1.4/list 1.12

--- src/test/java/junit/tutorial/CalculatorTest.java
+++ src/test/java/junit/tutorial/CalculatorTest.java
@@ -20,4 +20,18 @@ public class CalculatorTest {
         int actual = calculator.multiply(5, 7);
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    public void testDivideSuccessAs3by2() {
+        Calculator calculator = new Calculator();
+        float expected = 1.5f;
+        float actual = calculator.divide(3, 2);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideIllegalArdumentExceptionAs5by0() {
+        Calculator calculator = new Calculator();
+        calculator.divide(5, 0);
+    }
 }
