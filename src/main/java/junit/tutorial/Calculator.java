0826c581ecc63f486371786f650bbe81f529ecda
toms74209200 <motomotomato@gmail.com>
2021-01-09 23:19:47 +0900

    [add]add sec. 1.3

---
+++ src/main/java/junit/tutorial/Calculator.java
@@ -0,0 +1,12 @@
+package junit.tutorial;
+
+public class Calculator {
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+}
---
+++ src/test/java/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,12 @@
+package junit.tutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class CalculatorTest {
+    @Test
+    public void test() {
+        fail("Not implemented yet");
+    }
+}
